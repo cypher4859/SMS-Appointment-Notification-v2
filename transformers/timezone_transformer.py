2e59786484fc71d8058061c1165dc8442e597caf
Charles S. Kayser <charlesskayser@gmail.com>
2019-11-05 03:19:34 +0000

    Removed an unnecessary assertion

--- transformers/timezone_transformer.py
+++ transformers/timezone_transformer.py
@@ -4,7 +4,7 @@ class timezone_transformer:
 	def __init__(self, dateobj, tz):
 		self.date = dateobj
 		self.tz = tz
-		assert self.tz == 'US/Eastern', 'The timezone is wrong!'
+		#assert self.tz == 'US/Eastern', 'The timezone is wrong!'
 
 	def transform_utc_to_timezone_object(self):
 		return self.date.astimezone(timezone(self.tz))
